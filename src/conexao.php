80aefb5279d7d830a763c257a9703efaf56f9a4d
Maria Morena <mariasixmorena@gmail.com>
2026-05-16 23:02:12 -0300

    config postgres railway

--- src/conexao.php
+++ src/conexao.php
@@ -6,10 +6,54 @@ $user = getenv("PGUSER");
 $senha = getenv("PGPASSWORD");
 $port = getenv("PGPORT");
 
-$conexao = new PDO(
-    "pgsql:host=$host;port=$port;dbname=$db",
-    $user,
-    $senha
-);
+try {
+
+    $conexao = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$db",
+        $user,
+        $senha
+    );
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  
+    $conexao->exec("
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(100) NOT NULL,
+            senha VARCHAR(255) NOT NULL
+        )
+    ");
+
+
+    $conexao->exec("
+        CREATE TABLE IF NOT EXISTS despesas (
+            id SERIAL PRIMARY KEY,
+            descricao VARCHAR(255) NOT NULL,
+            valor DECIMAL(10,2) NOT NULL,
+            data DATE
+        )
+    ");
+
+ 
+    $verifica = $conexao->query("
+        SELECT COUNT(*) FROM usuarios
+    ");
+
+    if ($verifica->fetchColumn() == 0) {
+
+        $conexao->exec("
+            INSERT INTO usuarios (email, senha)
+            VALUES (
+                'admin@email.com',
+                md5('123456')
+            )
+        ");
+    }
+
+} catch(PDOException $e) {
+
+    echo "Erro: " . $e->getMessage();
+}
 
 ?>
