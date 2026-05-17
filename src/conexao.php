c3b098e1e0e06ea724aedaade2ebfccc37135692
Maria Morena <mariasixmorena@gmail.com>
2026-05-17 01:45:25 -0300

    fix: atualizando nome do banco para khxu

--- src/conexao.php
+++ src/conexao.php
@@ -1,10 +1,9 @@
 <?php
-$host = getenv('DB_HOST') ?: "localhost";
+$host = getenv('DB_HOST') ?: "dpg-d84jkj8g4nts73f8lmb0-a"; 
 $port = getenv('DB_PORT') ?: "5432";
-
 $db   = getenv('DB_NAME') ?: "controle_gastos_khxu"; 
-$user = getenv('DB_USER') ?: "postgres";
-$pass = getenv('DB_PASS') ?: "senha";
+$user = getenv('DB_USER') ?: "admin";
+$pass = getenv('DB_PASS') ?: "4eGccqYJaUSiGJh8pFJQHNYe3wcH2rSp"; 
 
 try {
     $conexao = new PDO(
